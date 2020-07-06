45c49d34ae2d084750d41f07e0d1e058ad7ab899
Angelo <angelo_1976@icloudcom>
2020-07-06 22:14:55 +0200

    repo

---
+++ test/app.js
@@ -0,0 +1,85 @@
+// // document.writeln ("Hello World");
+// // console.log("hello World");
+// alert ("Hello World")
+//
+//
+// document.getElementById('angel') .innerHTML="Hellos Angel World"
+
+// var miaStringa;
+// miaStringa='ciao';
+// alert (miaStringa);
+//
+// var miaStringa;
+// miaStringa='Arrivederci';
+// alert (miaStringa);
+
+//
+// var miaStringa;
+// miaStringa = 'Rosso' + ' ' +  'Acceso';
+// alert (miaStringa);
+//
+
+// var colore = 'Rosso';
+// var c = 'Acceso';
+//
+// var miaStringa = colore + ' ' + c;
+// alert (miaStringa);
+
+
+// var stringa = '8';
+// var numero = 8;
+//
+// console.log(stringa);
+// console.log(guseppe);
+
+
+// var somma = 4+4;
+// var sottrazione = somma - 2;
+// var moltiplicazione = somma * 2;
+// var divisione = somma /2;
+// var resto = 7 % 2;
+
+// var divisione = 7/2;
+// var d = 7 % 2;
+//
+// console.log(divisione);
+// console.log(d);
+
+// console.log(somma);
+// console.log(sottrazione);
+// console.log(moltiplicazione);
+// console.log(divisione);
+// console.log(resto);
+
+// var a = 6;
+// var b = 7;
+// var somma = a - b;
+//
+// console.log(somma);
+
+// var a = 6;
+// var b = 7;
+// a += b;
+//
+// console.log(a);
+
+// var text = prompt ("inseriesci il nome");
+// console.log(text);
+// var eta = parseInt(prompt ("inseriesci l \ ' eta"));
+// console.log(eta);
+
+
+// var nome = prompt ("inseriesi il tuo nome");
+// document .getElementById('saluto') .innerHTML = "un saluto" + " " +  nome;
+
+// var eta = parseInt (prompt ("quanti anni hai"));
+// var differnza = 2020 - eta;
+// document .getElementById('saluto') .innerHTML ="allora sei nato nel " + differnza;
+
+var nome = prompt ("inserisci il tuo nome");
+var cognome = prompt ("inserisci il tuo cognome");
+var colore = prompt ("dimmi il tuo colore preferito");
+var numero = 19;
+
+document.getElementById("psw") .innerHTML = nome + cognome + colore + numero;
+console.log("hai creato un password poco sicura");
